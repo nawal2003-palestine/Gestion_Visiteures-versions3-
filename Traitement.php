4b0f0daf80cb0f187fe040afddc69c12e64a4485
nawal2003-palestine <fouzianawal6@gmail.com>
2025-01-04 00:02:04 +0100

    Delete Traitement.php

--- Traitement.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once ("Visiteur.php") ;
-$action="index";
-if (isset($_GET['action'])
-    $action=$_GET['action'];
-switch ($action)
-{
-    case "sup": Supprimer ($_GET['id']); break;
-}
